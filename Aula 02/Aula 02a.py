517885140c016bf078507a1be3e23f47ad8478f0
carloshenrique11087@gmail.com <carloshenrique11087@gmail.com>
2026-03-18 20:50:54 -0300

    Aula 02

---
+++ Aula 02/Aula 02a.py
@@ -0,0 +1,35 @@
+from mypyc.irbuild.util import is_dataclass_decorator
+
+print("Hellow Word")
+
+#Calculo
+print(7 + 4 )
+print("7" + "4" )
+print("7 + 4 ")
+
+nome ="Carlos_HSD"
+idade =18
+peso = 68.4
+print (nome, idade, peso )
+print(f"Hellow {nome}!!!")
+
+#nome = input("Digite seu nome: ")
+print(f"Hellow {nome}!!!")
+#idade = int(input("Digite seu idade: "))
+#peso = float(input("Digite seu peso: "))
+
+#atividade
+nome = input("digite seu nome: ")
+print(f"Hellow {nome}!!!")
+
+dia = input("qual dia: ")
+mes = input("qual mes: ")
+ano = input("qual ano: ")
+
+print(f'{dia}/{mes}/{ano}')
+
+A = int(input("coloque um numero: "))
+B = int(input("coloque outro  numero: "))
+
+soma = (A + B)
+print(soma)
---
+++ Pasta/main.py
@@ -0,0 +1,42 @@
+#Teste de Mesa
+#Atividade 1
+print("Atividade 01")
+X = 5
+Y = 10
+Z = 5/10
+X = X + 1
+Y = Z
+Z = Y + X
+print(X)
+print(Y)
+print(Z)
+
+#Atividade 2
+print("Atividade 02")
+A = 2
+B = 3
+C = 2 + A
+B = A
+A = C
+A = A + 1
+B = C
+C = B
+B = A / 2
+print(A)
+print(B)
+print(C)
+
+#Atividade 3
+print("Atividade 03")
+
+x = 0
+y = 0
+z = 0
+Resultado = x+y+Z
+print(Resultado)
+
+x = 10
+y = 25
+z = 30
+Resultado = x+y+Z
+print(Resultado)
